7becc001ed70d550d10daf79352d9edc365e9402
Giuseppe Fabiano <gfabiano40@gmail.com>
2019-03-12 14:38:30 +0100

    simplified code adding the vector inside the header struct

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Debug)]
+struct Point(String, String);
+
+#[derive(Debug)]
 struct Header<'a> {
     name: &'a str,
     units: &'a str,
     min: &'a str,
     max: &'a str,
-    freq: &'a str
+    freq: &'a str,
+    points: Vec<Point>
 }
 
-#[derive(Debug)]
-struct Point(String, String);
+
 
 fn main() -> std::io::Result<()> {
     let file = File::open(env::args().nth(1).unwrap())?;
@@ -23,22 +26,17 @@ fn main() -> std::io::Result<()> {
     let mut header_str = String::new();
     buf_reader.read_line(&mut header_str)?;
     
-    let headers: Vec<Header> = header_str.split(",").map(|x| {
+    let mut headers: Vec<Header> = header_str.split(",").map(|x| {
         let mut x = x.split("|");
         Header {
             name: x.next().unwrap().trim_matches('"'),
             units: x.next().unwrap().trim_matches('"'),
             min: x.next().unwrap().trim(),
             max: x.next().unwrap().trim(),
-            freq: x.next().unwrap().trim()
+            freq: x.next().unwrap().trim(),
+            points: Vec::new()
         } 
     }).collect();
-
-    let mut points: Vec<Vec<Point>> = Vec::with_capacity(headers.len());
-
-    for _ in 0..headers.len() {
-        points.push(Vec::new());
-    }
 
     for line in buf_reader.lines() {
         let line = line?;
@@ -46,7 +44,7 @@ fn main() -> std::io::Result<()> {
         let istant = line_split.next().unwrap();
         for (i, val) in line_split.enumerate() {
             if i > 0 && val != "" {
-                points[i].push(
+                headers[i].points.push(
                     Point(
                         istant.to_string(),
                         val.to_string()
@@ -56,10 +54,10 @@ fn main() -> std::io::Result<()> {
         }
     }
     
-    for (i, variable) in headers.iter().enumerate() {
+    for (i, header) in headers.iter().enumerate() {
         if i > 1 {
-            println!("{} = [", variable.name);
-            for point in points[i].iter() {
+            println!("{} = [", header.name);
+            for point in header.points.iter() {
                 println!("  {} {}", point.0, point.1);
             }
             println!("];");
